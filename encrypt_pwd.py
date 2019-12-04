03ba120dee1734e1b78f3466463b97ac5fb108bf
Lauobrio42 <laurine.obriotbouvier@outlook.fr>
2019-12-04 10:20:44 +0100

    Il faut faire la liste de mot de passe

---
+++ encrypt_pwd.py
@@ -0,0 +1,20 @@
+import csv
+import os
+import math
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+dir = dir_path + '\\password.csv'
+
+
+def encrypt(tag, user_name, pwd, CURL):
+    string = ""
+    with open(dir, mode='a') as csv_pwd_db:
+        pwd = [ord(c) for c in pwd]
+        for x in range(0, len(pwd)):
+            test = (pwd[x] ** 3.141592653589793) / 111000
+            test = str(test) + ","
+            string += test
+
+        pwd_val = [tag, user_name, string[:-1], CURL]
+        writer = csv.writer(csv_pwd_db)
+        writer.writerow(pwd_val)
